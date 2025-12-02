36ebeed1900e26210a78fe3f288f90b600f73d84
Hoel Keromnes <hkeromne@c1r7p10.42lehavre.fr>
2025-12-02 20:25:45 +0100

    structured project

--- srcs/test.c
+++ srcs/test.c
@@ -7,8 +7,6 @@ int	main(int ac, char **av)
 	(void) av;
 
 	init_env();
-	env();
-	export(av);
 	cd(ac, av);
 	write(1, environ[get_env_var_id("PWD")], ft_strlen(environ[get_env_var_id("PWD")]));
 	free_env();
